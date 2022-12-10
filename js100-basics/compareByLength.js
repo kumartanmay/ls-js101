31adba828c507fa1f6497d3f3758f0f5a8a7913a
Kumar Tanmay <ktanmay@gmail.com>
2022-12-10 06:39:04 +0530

    compare by length

---
+++ js100-basics/compareByLength.js
@@ -0,0 +1,9 @@
+function compareByLength(str1, str2) {
+  if (str1.length < str2.length) return -1;
+  if (str1.length > str2.length) return 1;
+  return 0;
+}
+
+console.log(compareByLength('patience', 'perseverance')); // -1
+console.log(compareByLength('strength', 'dignity'));      //  1
+console.log(compareByLength('humor', 'grace'));           //  0
