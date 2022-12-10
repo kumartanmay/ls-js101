bb49bd8c7d4bad0c843ff0a1d9c5db4fa6bc67e5
Kumar Tanmay <ktanmay@gmail.com>
2022-12-10 06:40:41 +0530

    two programs: date.js and example.js

---
+++ js100-basics/example.js
@@ -0,0 +1,23 @@
+function clone(obj) {
+  let newObj = Object.assign({}, obj) //
+  return newObj;
+}
+
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
+};
+
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false
