354932f2e5fd775d92863f2e5e3fffa6831f40f1
Kumar Tanmay <ktanmay@gmail.com>
2022-12-19 14:01:16 +0530

    further exploration of E206.

--- js101/js101-small-E206.js
+++ js101/js101-small-E206.js
@@ -20,13 +20,44 @@ DATA STRUCTURE:
   Input: A string separated by spaces
   Ouput: String extracted from array of elements consisting of words in the input
 ALGO:
-
+  Split the input sentence by " " separator. Print the second last element of the array.
 */
 
-console.log(penultimate("last word") === "last"); // logs true
+/*
+Further Exploration
+
+Our solution ignored a couple of edge cases because we explicitly stated that 
+you didn't have to handle them: strings that contain just one word, and strings that contain no words.
+
+Suppose we need a function that retrieves the middle word of a phrase/sentence. 
+What edge cases need to be considered? How would you handle those edge cases without ignoring them? 
+Write a function that returns the middle word of a phrase or sentence. 
+It should handle all of the edge cases you thought of.
+
+PROBLEM:
+  Implicit: Input can be of any length. May or may not contain spaces
+  Mental model: Check if the word has space. If not, the word or blank space itself is the middle word. 
+  In case of blank, print "blank input. there is no middle word". If the count of words in the array is 
+  even, then the middle word is length / 2. When it is odd, then (length - 1)/2.
+DATA STRUCTURE & ALGO:
+  (1) Check if the input has blank space or not. 
+  (2) If not, check if it is a single word or not. If not, then print, input is blank
+*/
+
+console.log(penultimate("The last word") === "last"); // logs true
 console.log(penultimate("Launch School is great!") === "is"); // logs true
+console.log(penultimate("last") === "last"); // logs true
+console.log(penultimate(" ") === ""); // logs true
 
 function penultimate(word) {
   let array = word.split(' ');
-  return array[array.length - 2];
+  if (array.length === 1) return array[0];
+
+  // TIL: " ".split(" ") returns ['','']
+  else if (array.length % 2 === 0) {
+    return array[array.length / 2];
+  }
+  else {
+    return array[(array.length - 1) / 2];
+  }
 }
