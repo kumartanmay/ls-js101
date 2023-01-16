4534811bdce7e13875eda1d01c13fd8d9a3cf486
Kumar Tanmay <ktanmay@gmail.com>
2023-01-16 21:27:13 +0530

    removed the cases when it was giving undefined output

--- js101/js101-small-E210.js
+++ js101/js101-small-E210.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-/*
-Build a program that randomly generates Teddy's age, and logs it to the console. 
-Have the age be a random number between 20 and 120 (inclusive).
-
-Example:
-Teddy is 69 years old!
-
-Problem:
-How do you introduce a random number between a range?
-Explicit rule: The random number should be between 20 and 120, both inclusive
-Implicit rule: The output should be a whole number.
-
-Data Structure:
----------------
-Call Math.random() function and multiply it with 121. Floor it.
-If the age is less than 20, then call the function again.
-*/
-
-function age(max) {
-  return Math.floor(Math.random()* max);
-  //return Math.round(Math.random() * max);
-}
-
-function teddysAge(max) {
-  let teddysAge = age(max);
-  if (teddysAge < 20) {
-    teddysAge = age(max);
-  }
-  else if (teddyAge = undefined) {
-    return `UNDEFINED! ${age(max)}`;
-  }
-  else return `Teddy is ${teddysAge} years old`;
-}
-
-console.log(teddysAge(121));
-||||||| merged common ancestors
-=======
 /*
 Build a program that randomly generates Teddy's age, and logs it to the console. 
 Have the age be a random number between 20 and 120 (inclusive).
@@ -63,10 +25,8 @@ function teddysAge(max) {
   let teddysAge = age(max);
   if (teddysAge < 20) {
     teddysAge = age(max);
-  }
-
+  } 
   return `Teddy is ${teddysAge} years old`;
 }
 
 console.log(teddysAge(120));
->>>>>>> dev
