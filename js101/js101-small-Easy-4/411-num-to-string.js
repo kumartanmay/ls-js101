7f3af1589f69c91b78e0a0f4da5b7438494b18cc
Kumar Tanmay <ktanmay@gmail.com>
2023-04-10 23:01:16 +0530

    converts integer to string

---
+++ js101/js101-small-Easy-4/411-num-to-string.js
@@ -0,0 +1,7 @@
+function integerToString(num) {
+  return `${num}`;
+}
+console.log(integerToString(4321));        // "4321"
+console.log(integerToString(0));           // "0"
+console.log(integerToString(5000));        // "5000"
+console.log(integerToString(1234567890));  // "1234567890"
