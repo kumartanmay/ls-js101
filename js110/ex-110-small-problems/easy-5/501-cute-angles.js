daabcd776dfbc21fa4dfaf62d1159768112f66cd
Kumar Tanmay <ktanmay@gmail.com>
2023-04-17 14:58:46 +0530

    deg-minutes-secs-beyond-0-and-360

--- js110/ex-110-small-problems/easy-5/501-cute-angles.js
+++ js110/ex-110-small-problems/easy-5/501-cute-angles.js
@@ -17,6 +17,14 @@ dms(360);          // 360°00'00" or 0°00'00"
 */
 
 function dms(floatNum) {
+  while (floatNum < 0) {
+    floatNum += 360;
+  }
+
+  while(floatNum > 360) {
+    floatNum -= 360;
+  }
+
   let deg = parseInt(floatNum);
   let decimal = floatNum - parseInt(floatNum);
   let mins = minutes(decimal);
@@ -40,3 +48,19 @@ dms(254.6);        // 254°35'59"
 dms(93.034773);    // 93°02'05"
 dms(0);            // 0°00'00"
 dms(360);          // 360°00'00" or 0°00'00"
+dms(-420);         // 300°00'00"
+dms(-40);          // 320°00'00"
+dms(400);          // 40°00'00"
+
+
+/*
+Since degrees are normally restricted to the range 0-360, 
+can you modify the code so it returns a value in the appropriate range 
+when the input is less than 0 or greater than 360?
+
+dms(-1);   // 359°00'00"
+dms(400);  // 40°00'00"
+dms(-40);  // 320°00'00"
+dms(-420); // 300°00'00"
+*/
+
