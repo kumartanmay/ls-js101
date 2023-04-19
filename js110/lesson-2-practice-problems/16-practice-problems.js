3b99dd797f0d2f29253a00bdc1663691baa280ea
Kumar Tanmay <ktanmay@gmail.com>
2023-04-19 22:39:28 +0530

    create an object from nested array of paired elements

---
+++ js110/lesson-2-practice-problems/16-practice-problems.js
@@ -0,0 +1,23 @@
+/*
+Given the following data structure, 
+write some code that defines an object where the key is the first item in each subarray, 
+and the value is the second.
+
+let arr = [['a', 1], ['b', 'two'], ['sea', {'c': 3}], ['D', ['a', 'b', 'c']]];
+
+// expected value of object
+// { a: 1, b: 'two', sea: { c: 3 }, D: [ 'a', 'b', 'c' ] }
+
+*/
+
+let arr = [['a', 1], ['b', 'two'], ['sea', {'c': 3}], ['D', ['a', 'b', 'c']]];
+
+// expected value of object
+// { a: 1, b: 'two', sea: { c: 3 }, D: [ 'a', 'b', 'c' ] }
+
+let output = {};
+
+arr.forEach(elem => {
+  output[elem[0]] = elem[1];
+});
+console.log(output);
