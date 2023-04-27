5b4f09f5bf3ab01d4e529efa9c2889bb6584480f
Kumar Tanmay <ktanmay@gmail.com>
2023-04-27 12:35:51 +0530

    displays score  after each round

--- js110/lesson-3-larger-problems/tictactoe.js
+++ js110/lesson-3-larger-problems/tictactoe.js
@@ -13,9 +13,85 @@
 */
 
 const rlsync = require('readline-sync'); 
+const GAMES_TO_WIN_MATCH = 2;
+const BOARD_SIZE = 9;
+let score = { 'computerScore': 0, 'yourScore': 0};
+let gameCount = 0;
+
+prompt("Welcome to TicTacToe!");
+prompt(`How many games do you want to play in the series? (min ${GAMES_TO_WIN_MATCH} ): `)
+let seriesCount = Number(rlsync.question());
+
+while (seriesCount < GAMES_TO_WIN_MATCH) {
+  prompt(`Please enter a number greater than ${GAMES_TO_WIN_MATCH}: `);
+  seriesCount = Number(rlsync.question());
+}
+
+while (gameCount < seriesCount && (score['computerScore'] < GAMES_TO_WIN_MATCH && score['yourScore'] < GAMES_TO_WIN_MATCH)) {  // boolean for user's consent to play again
+  
+  let board = initializeBoard();
+  displayBoard(board); // display blank board
+  gameCount += 1;
+  //prompt(`Game Number: ${gameCount}`);
+
+  prompt(`Computer Score: ${score['computerScore']}`);
+  prompt(`Your Score: ${score['yourScore']}`);
+
+  while(true) {
+
+    prompt(`Game Number: ${gameCount}`);
+    prompt(`Computer Score: ${score['computerScore']}`);
+    prompt(`Your Score: ${score['yourScore']}`);
+
+    computerBoardMark(board);
+    if(someoneWon(board) || isFull(board)) break;
+
+    displayBoard(board);
+
+    userBoardMark(board); // ask for user input
+    if(someoneWon(board) || isFull(board)) break;
+
+    displayScore(board);
+    displayBoard(board);
+    
+    /*
+    if(score['computerScore'] === GAMES_TO_WIN_MATCH || score['yourScore'] === GAMES_TO_WIN_MATCH) {
+      prompt(`Computer Score: ${score['computerScore']}`);
+      prompt(`Your Score: ${score['yourScore']}`);
+      break;
+    } */
+
+  }
+
+  displayBoard(board); // display final output
+
+  if (someoneWon(board)) {
+    console.log(`${detectWinner(board)} won!`);
+  } else {
+    console.log(`it's a tie!`);
+  }
+
+  //prompt(`Game Number: ${gameCount}`);
+
+  displayScore(board, score);
+  prompt(`It was a series of ${seriesCount} games!`);
+  prompt(`Computer Score: ${score['computerScore']}`);
+  prompt(`Your Score: ${score['yourScore']}`);
+
+
+ // + Switching off this block of code
+ // + We are now allowing people to play a series of min 5 games
+
+  /*
+  prompt("Play again? ");
+  let consent = rlsync.question();
+  if(consent[0].toLowerCase() !== 'y') break; 
+  */
+
+}
 
 function displayBoard (squareMark) {
-  console.clear()
+  //console.clear()
   console.log(`     |     |`);
   console.log(`     |     |`);
   console.log(`  ${squareMark['1']}  |  ${squareMark['2']}  |  ${squareMark['3']}`);
@@ -34,7 +110,7 @@ function displayBoard (squareMark) {
 
 function initializeBoard () {
   let board = {};
-  for (let i = 1; i <= 9; i++) {
+  for (let i = 1; i <= BOARD_SIZE; i++) {
     board[String(i)] = ' ';
   }
   return board;
@@ -54,7 +130,7 @@ function userBoardMark (board) {
 
     if(emptySquares(board).includes(userMark)) break; 
     
-    prompt("That's not a valid choice");
+    prompt(`That square is already taken! Please enter one of the numbers: ${joinOr(emptySquares(board))}`);
     
   }
   
@@ -112,28 +188,6 @@ function detectWinner(board) {
   return null;
 }
 
-// main run:
-
-let board = initializeBoard();
-displayBoard(board); // display blank board
-
-while(true) {
-  displayBoard(board);
-
-  userBoardMark(board); // ask for user input
-  if(someoneWon(board) || isFull(board)) break;
-
-  computerBoardMark(board);
-  if(someoneWon(board) || isFull(board)) break;
-}
-displayBoard(board); // display the board after final input
-
-if (someoneWon(board)) {
-  console.log(`${detectWinner(board)} won!`);
-} else {
-  console.log(`it's a tie!`);
-}
-
 /*
 joinOr([1, 2, 3]);               // => "1, 2, or 3"
 joinOr([1, 2, 3], '; ');         // => "1; 2; or 3"
@@ -178,3 +232,58 @@ function joinOr(arr, delimiter = ", ", joinWord = "or ") {
 //console.log(joinOr([]));
 //console.log(joinOr([5]));
 
+/*
+KEEP SCORE
+==========
+
+Keep track of how many times the player and computer each win, and report the scores after each game. 
+The first player to win 5 games wins the overall match (a series of 2 or more games). 
+The score should reset to 0 for each player when beginning a new match. Don't use any global variables. 
+However, you may want to use a global constant to represent the number of games needed to win the match.
+
+Problem:
+=======
++ Number of games to be played is not known: take user input
++ How to track the game count?
++ Detect winner tells the winner: computer or user 
++ If it is tie, no one gets a score
++ First player to reach a score of 5, wins the match
++ Each series consists of two or more games. (Is it required? A winner cannot be decided before playing 5 games).
++ Score starts from zero
+
+Data Structure and Algo:
+========================
++ Input: 
+  + How many games you want to play (Min 5)?
++ Track 
+  + count of games played
+  + Continue with the series if game count is less than series count
+  + End series, if count is same as series count
+  + scores of players
++ Algo 
+  + scores of players after each game
+    + No change in score in case of a tie
+  + If score of either player is not 5 and count of games less than series, continue with the series
+  + Display winner if any one has a score = 5, series end.
+  + If game count is same as series count and no score is 5, then display scores (series end)
+
++ Output: 
+  + Scores of players after each game
+  + Winner: the first to score 5 wins
+*/
+
+function displayScore(board, score) {
+
+  switch(detectWinner(board)){
+    case 'Computer':
+      score['computerScore'] += 1;
+      break;
+    case 'You':
+      score['yourScore'] += 1;
+      break;
+    default:
+      break;
+  };
+
+  return score;
+}
