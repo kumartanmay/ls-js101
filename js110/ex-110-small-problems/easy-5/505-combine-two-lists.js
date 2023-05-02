8eaa4f4cfbaed999cf2a4d5f64dda3ec820e4937
Kumar Tanmay <ktanmay@gmail.com>
2023-05-02 08:28:50 +0530

    combine two lists

---
+++ js110/ex-110-small-problems/easy-5/505-combine-two-lists.js
@@ -0,0 +1,25 @@
+/*
+COMBINE TWO LISTS
+
+Write a function that combines two arrays passed as arguments, and r
+eturns a new array that contains all elements from both array arguments, with each element taken in alternation.
+
+You may assume that both input arrays are non-empty, and that they have the same number of elements.
+
+Example:
+interleave([1, 2, 3], ['a', 'b', 'c']);    // [1, "a", 2, "b", 3, "c"]
+
+*/
+
+function interleave(arrA, arrB) {
+  let result = [];
+
+  if(arrA.length === arrB.length) {
+    for(let i = 0; i < arrA.length; i++) {
+      result = result.concat(arrA[i], arrB[i]);
+    }
+  }
+  return result;
+}
+
+console.log(interleave([1, 2, 3], ['a', 'b', 'c']));
