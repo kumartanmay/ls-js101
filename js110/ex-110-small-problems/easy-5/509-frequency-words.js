70d3e039ffc1730f957433d7e40bdd89520e415a
Kumar Tanmay <ktanmay@gmail.com>
2023-05-03 08:55:11 +0530

    frequency of words in a list

---
+++ js110/ex-110-small-problems/easy-5/509-frequency-words.js
@@ -0,0 +1,30 @@
+/*
+Write a function that counts the number of occurrences of each element in a given array. 
+Once counted, log each element alongside the number of occurrences. 
+Consider the words case sensitive e.g. ("suv" !== "SUV").
+
+Example:
+let vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
+                'motorcycle', 'motorcycle', 'car', 'truck'];
+
+countOccurrences(vehicles);
+
+// console output -- your output sequence may be different
+car => 4
+truck => 3
+SUV => 1
+motorcycle => 2
+*/
+
+function countOccurrences (arr) {
+  let result = {};
+  arr.forEach(elem => {
+    return result[elem] ? result[elem] += 1 : result[elem] = 1;
+  })
+  return result;
+}
+
+let vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
+                'motorcycle', 'motorcycle', 'car', 'truck'];
+
+console.log(countOccurrences(vehicles));
