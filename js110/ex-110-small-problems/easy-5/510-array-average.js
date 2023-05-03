0632ddec3b0b309cab871564dcb9fa77b5ca460d
Kumar Tanmay <ktanmay@gmail.com>
2023-05-03 09:08:20 +0530

    used forEach method to find the average

--- js110/ex-110-small-problems/easy-5/510-array-average.js
+++ js110/ex-110-small-problems/easy-5/510-array-average.js
@@ -7,8 +7,18 @@ average([1, 5, 87, 45, 8, 8]);       // 25
 average([9, 47, 23, 95, 16, 52]);    // 40
 */
 
+/*
 function average(arr) {
   return parseInt(arr.reduce((acc, cur) => acc + cur, 0)/arr.length);
+}
+*/
+
+function average(arr) {
+  let sum = 0;
+  arr.forEach(elem => {
+    sum += elem;
+  })
+  return Math.floor(sum / arr.length);
 }
 
 console.log(average([1, 5, 87, 45, 8, 8]));
