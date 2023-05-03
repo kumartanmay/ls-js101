26e8d233d9f2b558aa8c76f0cb5241075ee08f45
Kumar Tanmay <ktanmay@gmail.com>
2023-05-03 09:04:42 +0530

    reduce method to find avg of numbers in an array

---
+++ js110/ex-110-small-problems/easy-5/510-array-average.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes one argument, an array of integers, and returns the average of all the integers in the array, 
+rounded down to the integer component of the average. The array will never be empty, and the numbers will always be positive integers.
+
+Examples:
+average([1, 5, 87, 45, 8, 8]);       // 25
+average([9, 47, 23, 95, 16, 52]);    // 40
+*/
+
+function average(arr) {
+  return parseInt(arr.reduce((acc, cur) => acc + cur, 0)/arr.length);
+}
+
+console.log(average([1, 5, 87, 45, 8, 8]));
+console.log(average([9, 47, 23, 95, 16, 52]));
