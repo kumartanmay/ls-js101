fade1abd2259fcd0800806e5414b8a647b3e8506
Kumar Tanmay <ktanmay@gmail.com>
2023-05-05 10:03:25 +0530

    reverse words greater than five letters in a sentence

---
+++ js110/ex-110-small-problems/easy-6/608-reverse-string.js
@@ -0,0 +1,27 @@
+/*
+Write a function that takes a string argument containing one or more words and returns a new string containing the words 
+from the string argument. All five-or-more letter words should have their letters in reverse order. 
+The string argument will consist of only letters and spaces. Words will be separated by a single space.
+
+reverseWords('Professional');             // "lanoisseforP"
+reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
+*/
+
+// create an array consisting of words in a sentence
+// reverse the letters of words with length > 5
+// return a new string with the modified words
+
+function reverseWords(sentence) {
+  let copySentence = sentence.slice(); // creates a copy of setence
+  let words = copySentence.split(" ").map(word => {
+    if (word.length > 4) return word.split('').reverse().join('');
+    return word;
+  });
+
+  console.log(words.join(" "));
+}
+
+reverseWords('Launch School');
+reverseWords('Professional');
+reverseWords('Walk around the block');
