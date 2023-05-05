91a467ec88598e80f45d015f8237fb0f4e03e40e
Kumar Tanmay <ktanmay@gmail.com>
2023-05-05 12:26:25 +0530

    sum of digits using methods calls only

---
+++ js110/ex-110-small-problems/list-processing/1-sum-of-digits.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes one argument, a positive integer, and returns the sum of its digits. 
+Do this without using for, while, or do...while loops - instead, use a series of method calls to perform the sum.
+
+Examples:
+sum(23);           // 5
+sum(496);          // 19
+sum(123456789);    // 45
+*/
+
+function sum(num) {
+  return num.toString().split("").reduce((acc, cur) => Number(acc) + Number(cur), 0);
+}
+
+console.log(sum(23));
+console.log(sum(496));
+console.log(sum(123456789));
