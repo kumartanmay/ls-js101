8a8b279911017f6453ed635bceb62fba9abaa0c7
Kumar Tanmay <ktanmay@gmail.com>
2023-05-06 05:37:58 +0530

    create an ordered list of substrings starting from first letter of the string

---
+++ js110/ex-110-small-problems/list-processing/4-leading-substrings.js
@@ -0,0 +1,30 @@
+/*
+Write a function that takes a string argument and returns a list of substrings of that string. 
+Each substring should begin with the first letter of the word, and the list should be ordered from shortest to longest.
+
+Examples:
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+*/
+
+function leadingSubstrings(str) {
+  /*
+  1. The resulting substring is combination of characters of the string
+  2. Possible Substrings are same in count as number of characters in a string
+
+  */
+
+  let arrayOfCharsInString = str.split(""); // [a, b, c]
+  let result = [arrayOfCharsInString[0]];
+
+  for(let i = 1; i < arrayOfCharsInString.length; i++) {
+    result[i] = result[i - 1] + arrayOfCharsInString[i]; 
+  }
+
+  return result;
+}
+
+console.log(leadingSubstrings('abc'));
+console.log(leadingSubstrings('xyzzy'));
+console.log(leadingSubstrings('a'));
