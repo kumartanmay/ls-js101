6ff184538b83e897474c6f198e2f929976084669
Kumar Tanmay <ktanmay@gmail.com>
2023-05-07 05:38:39 +0530

    ignore the non-alphabets; alternate the case of every alphabet b/w upper and lower case

---
+++ js110/ex-110-small-problems/string-and-text-processing/6-staggered-case-2.js
@@ -0,0 +1,34 @@
+/*
+Modify the function from the previous exercise so it ignores non-alphabetic characters when determining 
+whether it should uppercase or lowercase each letter. The non-alphabetic characters should still be included in the return value; 
+they just don't count when toggling the desired case.
+
+Example:
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+console.log(
+  staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+);
+*/
+
+function staggeredCase(str) {
+  let strArr = str.split("");
+  let nonCharStackIndex = [];
+  let charStackIndex = [];
+
+  strArr.forEach((char, index) => {
+    if (char.match(/[^A-Za-z]/i)) nonCharStackIndex.push(index);
+    else charStackIndex.push(index);
+  });
+
+  charStackIndex.map((elem, index) => {
+    if(index % 2 === 0) strArr[elem] = strArr[elem].toUpperCase();
+    else strArr[elem] = strArr[elem].toLowerCase();
+  })
+
+  return strArr.join("");
+
+}
+console.log(staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs");
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+console.log(staggeredCase("ALL CAPS") === "AlL cApS");
