f567ecd488819961b1c5f6e91421624cd217fbf4
Kumar Tanmay <ktanmay@gmail.com>
2023-05-07 05:56:04 +0530

    further explored whether non-alpha chars are counted

--- js110/ex-110-small-problems/string-and-text-processing/6-staggered-case-2.js
+++ js110/ex-110-small-problems/string-and-text-processing/6-staggered-case-2.js
@@ -11,24 +11,34 @@ console.log(
 );
 */
 
-function staggeredCase(str) {
+function staggeredCase(str, ignoreNonAlpha = true) {
   let strArr = str.split("");
-  let nonCharStackIndex = [];
-  let charStackIndex = [];
 
-  strArr.forEach((char, index) => {
-    if (char.match(/[^A-Za-z]/i)) nonCharStackIndex.push(index);
-    else charStackIndex.push(index);
-  });
+  if (ignoreNonAlpha){
+    let nonCharStackIndex = [];
+    let charStackIndex = [];
 
-  charStackIndex.map((elem, index) => {
-    if(index % 2 === 0) strArr[elem] = strArr[elem].toUpperCase();
-    else strArr[elem] = strArr[elem].toLowerCase();
-  })
+    strArr.forEach((char, index) => {
+      if (char.match(/[^A-Za-z]/i)) nonCharStackIndex.push(index);
+      else charStackIndex.push(index);
+    });
+
+    charStackIndex.map((elem, index) => {
+      if(index % 2 === 0) strArr[elem] = strArr[elem].toUpperCase();
+      else strArr[elem] = strArr[elem].toLowerCase();
+    })
+  }
+  else {
+    return strArr.map((elem, index) => {
+      if(index % 2 === 0) elem = elem.toUpperCase()
+      else elem = elem.toLowerCase();
+      return elem;
+    }).join("");
+  }
 
   return strArr.join("");
 
 }
-console.log(staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs");
-console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
-console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+console.log(staggeredCase("ignore 77 the 444 numbers"));
+console.log(staggeredCase("I Love Launch School!"));
+console.log(staggeredCase("ALL CAPS", false));
